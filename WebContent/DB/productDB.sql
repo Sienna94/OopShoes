d12ac054499715dd89bbd053cb796197a011b774
jsj94 <jsj94@DESKTOP-RUCQHIH>
2020-12-23 14:13:51 +0900

    new 목록 교체 / 제품 리스트

--- WebContent/DB/productDB.sql
+++ WebContent/DB/productDB.sql
@@ -76,38 +76,59 @@ VALUES (seq_pid.nextval, '펌프 코트 준지', '리복', 299000 , 100, 'H69059
 
 --반스--
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, 'Vans x Anderson Paak Old Skool 올드스쿨', '반스', 99000 , 100, 'VN0A4U3B2TA1_VN0A4U3B2TA1_primary(4).jpg', 'VN0A4U3B2TA1_VN0A4U3B2TA1_02.jpg', 'VN0A4U3B2TA1_VN0A4U3B2TA1_05.jpg', '오, 예! 캘리포니아의 느긋한 라이프스타일의 대명사인 Vasn와 앤더슨 팩이 드디어 이번 시즌, 음악적으로 영감을 받은 풋웨어와 어패럴의 스페셜 컬렉션을 위해 협업했습니다. 캘리포니아 옥스나드의 해안가 출신인 앤디는 삶에 대한 엄청난 사랑을 모든 작품마다 불어넣는데, 이번 컬렉션에서도 물론 예외는 아닙니다. Vans x Anderson Paak Old Skool 올드스쿨은 Vans의 상징적인 사이드스트라이프 스케이트 슈즈에 질감이 살아있는 텍스타일 소재의 어퍼, 반복적인 마모를 견디도록 보강된 토캡, 안정감과 유연성을 위해 패딩 처리된 칼라 및 시그니처 고무 와플 아웃솔을 결합한 모델입니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, '레트로 스포츠 스타일 36 STYLE 36', '반스', 75000 , 100, 'VN0A3DZ3VY11_VN0A3DZ3VY11_primary.jpg', 'VN0A3DZ3VY11_VN0A3DZ3VY11_02.jpg', 'VN0A3DZ3VY11_VN0A3DZ3VY11_05.jpg', '레트로 스포츠 Style 36 스타일 36은 Vans의 아이코닉한 사이드 스트라이프가 적용된 클래식 스케이트 슈즈입니다. 로우 탑 레이스업 형태로 내구성이 뛰어난 스웨이드, 캔버스 원단이 어퍼에 사용되었고, 반복적인 착용에도 견고한 형태를 유지하기 위해 토캡을 보강하였으며, 카라 부분에는 패드를 덧대 편안한 착용감을 제공합니다. Vans의 시그니처 와플솔이 적용되었습니다. (굽 높이: 3cm)', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, '레오파드 Era 에라', '반스', 65000 , 100, 'VN0A5EFN5DQ1_VN0A5EFN5DQ1_primary.jpg', 'VN0A5EFN5DQ1_VN0A5EFN5DQ1_02.jpg', 'VN0A5EFN5DQ1_VN0A5EFN5DQ1_05.jpg', '레오파드 Era 에라는 가죽 소재의 남녀공용 스케이트 슈즈입니다. 에라는 스케이트보더가 스케이트를 위해 디자인한 최초의 슈즈였습니다. 1976년에 반스 #95라는 이름으로 처음 등장한 에라는 전설적인 지-보이즈(Z-Boys)를 통해 인기를 얻어 현재까지도 전 세계 스케이터, 서퍼 및 창의적인 사람들이 즐겨 신는 슈즈로 남아있습니다. 이번 시즌, 반스는 전설적인 클래식 에라의 실루엣에 현대적인 해석으로 새로운 힘을 불어넣습니다. 견고한 텍스타일 소재 어퍼, 메탈 아일렛, 지지력과 유연성을 위해 패딩 처리된 칼라, 그리고 시그니처 고무 와플 아웃솔을 적용했습니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, 'Vans & MoMA SK8-Hi 스케이트-하이', '반스', 109000 , 100, 'VN0A4U3C22J1_VN0A4U3C22J1_primary(1).jpg', 'VN0A4U3C22J1_VN0A4U3C22J1_02.jpg', 'VN0A4U3C22J1_VN0A4U3C22J1_05.jpg', 'Vans와 뉴욕현대미술관(MoMA)이 협업하여, MoMA가 소장하고 있는 유명 아티스트의 작품을 풋웨어와 어패럴 컬렉션에 담았습니다. 뉴욕현대미술관과 Vans는 전세계적으로 창조적 자기표현과 개성을 고취하는 일에 대한 열정을 공유하고 있습니다. Vans & MoMA Sk8-Hi 스케이트-하이는 Vans의 전설적인 레이스-업 하이탑 스타일에 류보프 포포바(Lyubov Popova)의 아방가르드한 작품이 돋보이는 올오버 프린팅이 결합되었습니다. 또한, Vans & MoMA Sk8-Hi 스케이트-하이는 반복적인 마모를 견디도록 보강된 토캡과 안정감과 유연성을 위해 패딩 처리된 칼라, 시그니처 고무 와플 아웃솔이 특징입니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, '퓨처 스탠다드 SK8-Hi 스케이트-하이', '반스', 95000 , 100, 'VN0A3WMB2QE1_VN0A3WMB2QE1_primary.jpg', 'VN0A3WMB2QE1_VN0A3WMB2QE1_02.jpg', 'VN0A3WMB2QE1_VN0A3WMB2QE1_05.jpg', 'Vans가 클래식 스케이트-하이 실루엣에 컴피쿠시 기술을 적용해 새로운 숨을 불어넣었습니다. 더 부드럽고 쿠션감 있는 아웃솔을 사용한 퓨처 스탠다드 SK8-Hi 스케이트-하이는 구름 위를 걷는 듯한 최상급의 핏감을 제공합니다. 신발의 전체적인 형태와 고무를 조화롭게 몰딩해 편안함과 그립력 모두를 제공하는 퓨처 스탠다드 SK8-Hi 스케이트-하이는 또한 고무 아웃솔을 사용해 훌륭한 내구성과 마찰력을 실현하며, 일체형 내부 구조에 아치형 서포트 기술을 더하고, 신발 내부 전체에 습기를 흡수하는 안감을 사용했습니다. 새롭게 구조화된 스웨이드 및 캔버스 소재의 어퍼로 설포 부분의 안정성에 집중한 퓨처 스탠다드 SK8-Hi 스케이트-하이는 Vans의 90년대 초반 스포츠 스타일의 레트로한 감각에 편안한 착화감을 선사합니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, 'SK8-Hi 스케이트-하이 리이슈 CAP', '반스', 95000 , 100, 'VN0A3WM15FG1_VN0A3WM15FG1_primary.jpg', 'VN0A3WM15FG1_VN0A3WM15FG1_02.jpg', 'VN0A3WM15FG1_VN0A3WM15FG1_05.jpg', 'Vans가 클래식 스케이트-하이 실루엣에 컴피쿠시 기술을 적용해 새로운 숨을 불어넣었습니다. 더 부드럽고 쿠션감 있는 아웃솔을 사용한 퓨처 스탠다드 SK8-Hi 스케이트-하이는 구름 위를 걷는 듯한 최상급의 핏감을 제공합니다. 신발의 전체적인 형태와 고무를 조화롭게 몰딩해 편안함과 그립력 모두를 제공하는 퓨처 스탠다드 SK8-Hi 스케이트-하이는 또한 고무 아웃솔을 사용해 훌륭한 내구성과 마찰력을 실현하며, 일체형 내부 구조에 아치형 서포트 기술을 더하고, 신발 내부 전체에 습기를 흡수하는 안감을 사용했습니다. 새롭게 구조화된 스웨이드 및 캔버스 소재의 어퍼로 설포 부분의 안정성에 집중한 퓨처 스탠다드 SK8-Hi 스케이트-하이는 Vans의 90년대 초반 스포츠 스타일의 레트로한 감각에 편안한 착화감을 선사합니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, 'SK8-Hi 스케이트-하이 고어-텍스', '반스', 139000 , 100, 'VN0A4VJD23G1_VN0A4VJD23G1_primary.jpg', 'VN0A4VJD23G1_VN0A4VJD23G1_02.jpg', 'VN0A4VJD23G1_VN0A4VJD23G1_07.jpg', '고어-텍스(Gore-Tex®)는 가혹한 기상 환경에서 보호 성능과 편안함을 극대화한 제품을 개발하기 위해 노력합니다. 고어-텍스® 패브릭으로 설계한 제품은 가혹한 환경으로부터 착용자를 보호하여 뛰어난 성능, 내구성, 통기성 및 방수성을 보장합니다. 고어-텍스®와 스웨이드 소재의 어퍼를 적용한 SK8-Hi 스케이트-하이 고어-텍스는 반복적인 마모를 견디기 위한 강화 토캡, 지지력과 유연성을 제공하는 패딩 처리된 칼라, 그리고 Vans의 시그니처 고무 와플 아웃솔을 적용했습니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, 'Vans x Anderson Paak Old Skool 올드스쿨', '반스', 99000 , 100, 'VN0A4U3B2N91_VN0A4U3B2N91_primary.jpg', 'VN0A4U3B2N91_VN0A4U3B2N91_hover.jpg', 'VN0A4U3B2N91_VN0A4U3B2N91_05.jpg', '오, 예! 캘리포니아의 느긋한 라이프스타일의 대명사인 Vasn와 앤더슨 팩이 드디어 이번 시즌, 음악적으로 영감을 받은 풋웨어와 어패럴의 스페셜 컬렉션을 위해 협업했습니다. 캘리포니아 옥스나드의 해안가 출신인 앤디는 삶에 대한 엄청난 사랑을 모든 작품마다 불어넣는데, 이번 컬렉션에서도 물론 예외는 아닙니다. Vans x Anderson Paak Old Skool 올드스쿨은 Vans의 상징적인 사이드스트라이프 스케이트 슈즈에 질감이 살아있는 텍스타일 소재의 어퍼, 반복적인 마모를 견디도록 보강된 토캡, 안정감과 유연성을 위해 패딩 처리된 칼라 및 시그니처 고무 와플 아웃솔을 결합한 모델입니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, '스웨이드 볼드 엔아이', '반스', 79000 , 100, 'VN0A3WLPEMI1_VN0A3WLPEMI1_primary(4).jpg', 'VN0A3WLPEMI1_VN0A3WLPEMI1_02.jpg', 'VN0A3WLPEMI1_VN0A3WLPEMI1_05.jpg', '현대적인 감각으로 새롭게 선보이는 스타일, 볼드 엔아이 Bold Ni 입니다. 업그레이드된 폭싱 테이프와 스웨이드 업퍼에 빈티지한 감성을 담은 볼드 엔아이는, 슬림하게 디자인된 T자 형태의 토 쉐이프 디자인, 업퍼와 대비되는 색상의 개성있는 스티칭, 텅 부분의 라벨을 과감하게 없앤 것이 특징입니다. 또한, 삭스라이너의 울트라쿠시 UltraCush 라이트를 적용하여, 더욱 편안한 착용감을 제공합니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, '애너하임 Old Skool 올드스쿨 36 DX', '반스', 95000 , 100, 'VN0A38G2U8R1_VN0A38G2U8R1_primary(4).jpg', 'VN0A38G2U8R1_VN0A38G2U8R1_02.jpg', 'VN0A38G2U8R1_VN0A38G2U8R1_05.jpg', '애너하임 Old Skool 올드스쿨 36 DX는 캘리포니아주 애너하임 지역에 위치한 Vans의 첫 번째 공장을 기념하기 위해 만든 모델입니다. 오리지널 올드스쿨의 디테일을 계승하는 동시에 업그레이드된 OrthoLite® 삭라이너 기술력으로 모던하고 편안한 착용감을 더했습니다. 애너하임 Old Skool 올드스쿨 36 DX는 오리지널 스타일 넘버와 컬러 팔레트 또는 기존에 사용된 프린트와 같은 초창기에 선보였던 오리지널 디테일, 높고 은은한 광택이 매력적인 폭싱 테이프, 면 소재 레이스, 오리지널 드릴 라이닝 웨이트, 스웨이드 및 텍스타일 소재의 어퍼 등을 적용하여 룩을 완성했습니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, 'SK8-Hi 스케이트-하이 리이슈 CAP', '반스', 95000 , 100, 'VN0A3WM16BT1_VN0A3WM16BT1_primary.jpg', 'VN0A3WM16BT1_VN0A3WM16BT1_03.jpg', 'VN0A3WM16BT1_VN0A3WM16BT1_05.jpg', 'DIY와 컷 앤 페이스트 방식(Cut-and-Paste: 이미지들을 자르고 결합하여 새로운 이미지를 창조하는 방식)으로 평범함의 경계를 넘어선 컷 앤 페이스트 컬렉션입니다. 컷 앤 페이스트 SK8-Hi 스케이트-하이 리이슈는 Vans의 가장 사랑받는 실루엣 중 하나이자 전설적인 하이 탑 실루엣에서 영감을 받은, 스케이트-하이 SK8-Hi 에 다양한 소재와 반스의 아이코닉한 체커보드 패턴, 그리고 스티치를 붙여넣었습니다. 사이드에는 지퍼를 더했으며, 어퍼는 스웨이드와 캔버스 피스를 활용하여 인사이드-아웃(inside-out) 느낌을 살렸습니다. 패드에는 카라를 덧대어 편안한 착용감을 제공합니다. Vans의 시그니처 와플 아웃솔이 적용되었습니다.', SYSDATE);
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (seq_pid.nextval, '', '',  , 100, '.jpg', '.jpg', '.jpg', '', SYSDATE);
+VALUES (seq_pid.nextval, '애너하임 체커 콤보 SK8-Hi 스케이트-하이 38 DX', '반스', 99000 , 100, 'VN0A38GF2U71_VN0A38GF2U71_primary.jpg', 'VN0A38GF2U71_VN0A38GF2U71_02.jpg', 'VN0A38GF2U71_VN0A38GF2U71_05.jpg', '애너하임 체커 콤보 SK8-Hi 스케이트-하이 38 DX는 캘리포니아주 애너하임 지역에 위치한 Vans의 첫 번째 공장을 기념하기 위한 제품으로, 오리지널 Sk8-Hi 스케이트-하이의 디테일에 업그레이드된 오솔라이트(Ortholite®) 삭라이너 기술로 현대적인 착화감을 더했습니다. 애너하임 체커 콤보 SK8-Hi 스케이트-하이 38 DX는 오리지널 스타일 넘버와 컬러감이나 프린팅, 높고 은은한 광택이 돋보이는 폭싱 테이프, 면 소재의 신발끈, 오리지널 드릴 라이닝 웨이트, 스웨이드 및 섬유 소재의 어퍼로 룩을 완성합니다.', SYSDATE);
 
-
-
-
-
+--아디다스--
 INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
-VALUES (5, '에어', '뉴발란스', 75000, 23, 'image1.jpg', 'image2.jpg', 'image3.jpg', '5상세한설명입니다', SYSDATE);
-INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE) 
-VALUES (6, '에어2', '휠라', 75000, 23, 'image1.jpg', 'image2.jpg', 'image3.jpg', '6상세한설명입니다', SYSDATE);
+VALUES (seq_pid.nextval, 'Y-3 시쿠 런', '아디다스', 549000 , 100, 'FX1414-01-01_320X320.jpg', 'FX1414-02-02_720X720.jpg', 'FX1414-02-05_720X720.jpg', '아디다스의 대담한 정체성을 담아낸 Y-3 슈즈를 만나보세요.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, '슈퍼스타 W', '아디다스', 109000 , 100, 'FW3694-01-01_320X320.jpg', 'FW3694-02-02_720X720.jpg', 'FW3694-02-05_720X720.jpg', '한 가지로 정의할 수 없는 아이콘. 다양한 문화를 포용하는 50년 역사와 함께 그 어느때보다 강렬한 메시지를 전달하는 아디다스 슈퍼스타를 만나보세요. 컬러풀한 트레포일을 강조하는 깔끔하고 산뜻한 디자인의 러버 쉘토 슈즈입니다. 다양성과 관용을 상징하는 6가지 색상으로 모든 이를 위한 다채로운 룩을 선사합니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, '크레이지카오스 쉐도우', '아디다스', 99000 , 100, 'FX8894-01-01_320X320.jpg', 'FX8894-02-02_720X720.jpg', 'FX8894-02-05_720X720.jpg', '혼돈에서 영감을받은 디자인으로 일상을 흔들어보세요. 이 스니커즈는 혼합 된 소재, 레이어링 및 색상을 사용하여 클래식 한 러닝 룩에 신선한 스타일을 선보이고 있습니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, '오즈위고', '아디다스', 129000 , 100, 'FV9656-01-01_320X320.jpg', 'FV9656-02-02_720X720.jpg', 'FV9656-02-05_720X720.jpg', '새로운 미래의 시작을 알린 전설적인 슈즈. 러닝화로 탄생한 아디다스 오즈위고의 혁신적인 스타일과 기능을 계승하는 초현대적인 슈즈입니다. 과거에서 부활한 오리지널 디자인과 독보적인 쿠셔닝으로 스트리트를 위한 우수한 편안함을 선사합니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, '나이트볼', '아디다스', 139000 , 100, 'FV4842-01-01_320X320.jpg', 'FV4842-02-02_720X720.jpg', 'FV4842-02-05_720X720.jpg', '코트와 스트리트를 지배하는 나만의 스타일. 농구에서 영감을 얻은 대담하고 스포티한 슈즈입니다. 어두운 곳에서도 시선을 사로잡는 반사 디테일이 강렬한 존재감을 발산합니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, '니짜 X 디즈니 스포츠 구피', '아디다스', 69000 , 100, 'FW0645-01-01_320X320.jpg', 'FW0645-02-02_720X720.jpg', 'FW0645-02-05_720X720.jpg', '세계에서 가장 큰 스포츠 축제를 맞이하여 탄생한 콜라보레이션 슈즈를 만나보세요. 다양한 스포츠를 즐기는 역동적인 구피 프린트가 돋보이는 아디다스 니짜입니다. 클래식 벌커나이즈 디자인으로 편안한 착용감과 견고한 내구성을 선사합니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, 'AH-05 HI 시유레이터 GTX', '아디다스', 329000 , 100, 'FY6856-01-01_320X320.jpg', 'FY6856-02-02_720X720.jpg', 'FY6856-02-05_720X720.jpg', '대담하고 우아한 미니멀리스트 디자인 철학을 녹여낸 아디다스 x HYKE 콜라보레이션 컬렉션을 만나보세요. 기존 스니커즈 형태를 새로운 차원으로 끌어올린 90년대 오리지널 SEEULATER 슈즈의 새로운 버전입니다. 아웃도어에서 영감을 얻은 독특한 디테일과 함께 두 브랜드의 협업을 상징하는 혁신적인 실루엣을 선보입니다. 단단한 핏을 더해주는 벨트와 버클, 방수 고어텍스 안감이 견고한 구조를 완성합니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, 'Y-3 노토마', '아디다스', 599000 , 100, 'FX1768-01-01_320X320.jpg', 'FX1768-02-02_720X720.jpg', 'FX1768-02-05_720X720.jpg', '기존의 인식을 바꾸는 과감한 접근. 클래식 스포츠웨어를 정교하게 재해석한 Y-3 컬렉션을 만나보세요. 깔끔하고 심플한 디자인이 돋보이는 매끈한 슈즈입니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, 'Y-3 러너 4D', '아디다스', 699000 , 100, 'FX1058-01-01_320X320.jpg', 'FX1058-02-02_720X720.jpg', 'FX1058-02-05_720X720.jpg', '기존의 인식을 바꾸는 과감한 접근. 클래식 스포츠웨어를 정교하게 재해석한 Y-3 컬렉션을 만나보세요. 깔끔하고 심플한 디자인이 돋보이는 매끈한 슈즈입니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, 'Y-3 유벤 미드', '아디다스', 399000 , 100, 'FX0568-01-01_320X320.jpg', 'FX0568-02-02_720X720.jpg', 'FX0568-02-05_720X720.jpg', 'Y-3 컬렉션의 신선하고 독창적인 하드우드 스타일을 담아낸 미드컷 슈즈를 만나보세요. 농구화 실루엣에서 진화한 미니멀리스트 디자인으로 심플하고 깔끔한 룩을 선사합니다. 대비색상의 뒤꿈치 디테일이 돋보이는 가죽 갑피가 부드러운 착용감을 제공합니다. 설포의 Y-3 로고가 절제된 액센트를 더해줍니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, 'Y-3 히코', '아디다스', 399000 , 100, 'FX1752-01-01_320X320.jpg', 'FX1752-02-02_720X720.jpg', 'FX1752-02-05_720X720.jpg', '기존의 인식을 바꾸는 과감한 접근. 클래식 스포츠웨어를 정교하게 재해석한 Y-3 컬렉션을 만나보세요. 깔끔하고 심플한 디자인이 돋보이는 매끈한 슈즈입니다. 높은 굽의 두툼한 플랫폼이 대담하면서도 절제된 룩을 더해줍니다. 가죽 갑피가 부드러운 착용감을 선사합니다.', SYSDATE);
+INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+VALUES (seq_pid.nextval, 'Y-3 가이와', '아디다스', 599000 , 100, 'FX0909-01-01_320X320.jpg', 'FX0909-02-02_720X720.jpg', 'FX0909-02-05_720X720.jpg', '기존의 인식을 바꾸는 과감한 접근. 클래식 스포츠웨어를 정교하게 재해석한 Y-3 컬렉션을 만나보세요. 깔끔하고 심플한 디자인이 돋보이는 매끈한 슈즈입니다.', SYSDATE);
+
+--INSERT INTO PRODUCT(pID, pNAME, pBRAND, pPRICE, pSTOCK, pIMAGE1, pIMAGE2, pIMAGE3, pDETAIL, pRDATE)
+--VALUES (seq_pid.nextval, 'Y-3 유누 슈즈', '아디다스', 379000 , 100, 'FX0791-01-01_320X320.jpg', 'FX0791-02-02_720X720.jpg', 'FX0791-02-05_720X720.jpg', '기존의 인식을 바꾸는 과감한 접근. 클래식 스포츠웨어를 정교하게 재해석한 Y-3 컬렉션을 만나보세요. 깔끔하고 심플한 디자인이 돋보이는 매끈한 슈즈입니다.', SYSDATE);
+
 
 
 
